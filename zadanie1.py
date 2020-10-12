d9cde2eee22d483d5f70efd3adfa32b6983a1d3b
Nikita <nikprotsenko@mail.ru>
2020-10-12 21:36:23 +0300

    add zadanie2.py in zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -1,15 +1,23 @@
-# Решение задания
+# Решение задания 1
 with open('text_zadanie.txt', 'a') as f_obj:
     while True:
         user_str = input(
-            "Введите ваше предложение и нажмите Enter, чтобы прекратить работу программы просто нажмите Enter")
+            "Введите ваше предложение и нажмите Enter, чтобы прекратить работу программы просто нажмите Enter ")
         if user_str == '':
             print('Завершение программы!')
             break
         else:
             f_obj.write(user_str + '\n')
 
-# Проверка
+# Задание 2
 with open('text_zadanie.txt', 'r') as f_obj:
-    content = f_obj.read()
-    print(content)
+    count_str = 0
+    content = f_obj.readlines()
+    for letters in content:
+        count_str +=1
+        count_word = 0
+        for element in letters.split():
+            count_word +=1
+        print(f'В строке "{letters}"  {count_word} слов')
+    print(f'Всего строк в файле {count_str}')
+
