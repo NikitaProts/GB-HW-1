bd045ef1fd3092962d45ca24d44ac3f2558c3f45
Nikita <nikprotsenko@mail.ru>
2020-10-13 22:42:19 +0300

    add zadanie6.py

--- zadanie6.py
+++ zadanie6.py
@@ -2,11 +2,17 @@ with open('text_6.txt', 'r', encoding='utf-8') as zadanie6_txt:
     content = zadanie6_txt.readlines()
     dict_items = {}
     for line in content:
-        for element in line.split('('):
-            print(element)
+        item_name = line.partition(' ')[0]
+        sum_number = 0
+        number = 0
+        for element in line:
             try:
-                element = float(element)
-                if isinstance(element, float):
-                    print(element)
+                element = int(element)
+                if isinstance(element, int):
+                    number *= 10
+                    number += element
             except BaseException:
-                continue
+                sum_number += number
+                number = 0
+        dict_items.update({f'{item_name}': sum_number})
+print(dict_items)
